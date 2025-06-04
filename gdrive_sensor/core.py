ad003deef0d699d285af4c45fced5b4dc71c3ac7
lukvmil <millerluke1364@gmail.com>
2025-06-04 22:58:45 +0800

    reverted to default handlers, set handlers to process internal knowledge, added notes

--- gdrive_sensor/core.py
+++ gdrive_sensor/core.py
@@ -1,10 +1,5 @@
 import logging
 from koi_net import NodeInterface
-from koi_net.processor.default_handlers import (
-    basic_rid_handler,
-    edge_negotiation_handler,
-    basic_network_output_filter
-)
 from .config import ROOT, GDriveSensorNodeConfig
 from pprint import pprint
 
@@ -15,11 +10,6 @@ node = NodeInterface(
     use_kobj_processor_thread=True,
     # cache_directory_path=f"{ROOT}/net/metadata/gdrive_sensor_node_rid_cache",
     # identity_file_path=f"{ROOT}/net/metadata/gdrive_sensor_node_identity.json",
-    handlers=[
-        basic_rid_handler,
-        edge_negotiation_handler,
-        basic_network_output_filter
-    ]
 )
 
 from . import handlers
